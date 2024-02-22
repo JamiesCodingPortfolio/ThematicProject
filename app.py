52af71faa2d2f3e1ac31f8fa9b65fe8da2fc2432
JosephAshworth <JOSEPH.ASHWORTH@stu.mmu.ac.uk>
2024-02-22 11:10:22 +0000

    Added starter code

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
-import os
-
 import discord
-from dotenv import load_dotenv
+from discord.ext import commands
+
+# Define the intents your bot will use
+intents = discord.Intents.default()
+intents.messages = True  # Enable the ability to receive message events
+
+# Initialize the Bot object with the defined intents and command prefix
+bot = commands.Bot(command_prefix='boom!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(bot))
+
+@bot.command()
+async def hello(ctx):
+    print('Hello command executed')
+    await ctx.send('Hello {0.author.mention}!')
+
+
+
+bot.run('MTIxMDE1MjE2NzA0NzE3NjI2Mg.GOzzpL.IhrtlucFhSe0mGb6XYcvxTxVTmqGnp60U4cQAY')

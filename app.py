7641776687c54adefe9022767ba2a54dfc572def
JosephAshworth <JOSEPH.ASHWORTH@stu.mmu.ac.uk>
2024-02-22 12:10:24 +0000

    First message

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 
 # Define the intents your client will use
-intents = discord.Intents.default()
+intents = discord.Intents.all()
+intents.messages = True  # Enable the ability to receive message events
 
 # Initialize the Client object with the defined intents
 bot = commands.Bot(command_prefix='boom!', intents=intents)
@@ -13,5 +14,8 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
-    print('Hello command executed')
-    await ctx.send('Hello {0.author.mention}!')
+    await ctx.send('Hello {0.author.mention}!'.format(ctx))
+
+
+# Run the bot with your bot token
+bot.run('INSERT BOT TOKEN HERE')

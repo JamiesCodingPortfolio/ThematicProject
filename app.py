25fffcac430c32d3bdb81d25f377b42fe9485858
JosephAshworth <JOSEPH.ASHWORTH@stu.mmu.ac.uk>
2024-02-22 11:30:16 +0000

    starter code

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import discord
+from discord.ext import commands
 
-# Define the intents your bot will use
+# Define the intents your client will use
 intents = discord.Intents.default()
 intents.messages = True  # Enable the ability to receive message events
 
-# Initialize the Bot object with the defined intents and command prefix
+# Initialize the Client object with the defined intents
 bot = commands.Bot(command_prefix='boom!', intents=intents)
 
 @bot.event
@@ -15,3 +16,5 @@ async def on_ready():
 async def hello(ctx):
     print('Hello command executed')
     await ctx.send('Hello {0.author.mention}!')
+
+bot.run('MTIxMDE1MjE2NzA0NzE3NjI2Mg.GxNgJN.EEDDAXX8XGAxBh5NLE40pTzXoYHhVQI__UDb-0')

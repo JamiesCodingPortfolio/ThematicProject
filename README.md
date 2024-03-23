b6ef5bf7465725180e81b71a9387bb59ebe904a0
JamiesCodingPortfolio <jamie.brown.09001@gmail.com>
2024-03-23 10:27:27 +0000

    Added variables file to make changes easier

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # DiscordBot
 Uses the discordpy library.
 
-## Dependencies
+### Dependencies
 https://discordpy.readthedocs.io/en/stable/
+
 https://www.mongodb.com/docs/manual/
+
+### Running the bot on your local system
+- `MONGODBPATH` by default uses the localhost connection string - change if you're using anything more than the default connection string
+- `BOT_TOKEN` make sure to add your personal bot token
--- dbAccess.py
+++ dbAccess.py
@@ -16,6 +16,24 @@ if not database_access.is_file():
     logging.error("Token file not found.")
     exit()
 
+MONGODBPATH = ''
+
+with open("variables.txt", "r") as file:
+        # Read the content of the file
+    content = file.read()
+
+# Split the content by '=' to separate the variable name and its value
+variable_name, value = content.split('=')
+
+# Remove any leading or trailing whitespace from the value
+value = value.strip()
+
+# Create the variable dynamically using globals()
+globals()[variable_name.strip()] = value
+
+# Test the variable
+print(MONGODBPATH)
+
 with open(database_access, 'r') as file:
     database_access_url = file.read()
 

4b99c0e0857e4d4b390456be6f972a18e455cbf9
JosephAshworth <JOSEPH.ASHWORTH@stu.mmu.ac.uk>
2024-04-22 18:07:23 +0100

    Update on remove messages because i didnt want it to display a success message after deleting messages

--- adminCommands/remove_messages.py
+++ adminCommands/remove_messages.py
@@ -10,30 +10,19 @@ class remove_messages(commands.Cog):
     @app_commands.command(name="remove_messages", description="Removes a specific number of messages from the channel")
     async def remove_messages(self, interaction: discord.Interaction, number: int): 
 
-        # Defer the interaction first
-        await interaction.response.defer()
-
         # Check if the provided number is greater than zero. If not, send an error message and terminate the command
         if number <= 0:
-            await interaction.followup.send(content="Please provide a positive number greater than zero.", ephemeral=True)
+            await interaction.response.send_message("Please provide a positive number greater than zero.", ephemeral=True)
             return
         
         try:
             # Attempt to delete the specified number of messages plus the command message itself
-            deleted = await interaction.channel.purge(limit=number)
-            
-            # Check if messages were deleted successfully
-            if len(deleted) == number:
-                # Send a confirmation message that the number of messages have been deleted from the same channel
-                await interaction.followup.send(content=f"Successfully removed {number} messages.", ephemeral=True)
-
-            else:
-                await interaction.followup.send(content="Failed to remove messages. Please make sure I have permission to delete messages.", ephemeral=True)
+            await interaction.channel.purge(limit=number+1)
 
         # Include an except block to handle exceptions
         except discord.Forbidden:
-            await interaction.followup.send(content="I don't have permission to delete messages.", ephemeral=True)
+            await interaction.response.send_message("I don't have permission to delete messages.", ephemeral=True)
 
 # Define a setup function to add the cog to the bot 
-async def setup(client:commands.Bot) -> None:
+async def setup(client: commands.Bot) -> None:
     await client.add_cog(remove_messages(client))
